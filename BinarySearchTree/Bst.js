d59844c1271eb2c7a04dea263826f7e13ad37c88
yasinvural <yasin.vural@metu.edu.tr>
2019-04-02 21:15:33 +0300

    Update Bst.js

--- BinarySearchTree/Bst.js
+++ BinarySearchTree/Bst.js
@@ -1,33 +1,33 @@
 class Bst {
   constructor(data) {
-	this.data = data;
+        this.data = data;
 	this.left = null;
 	this.right = null;
   }
 
   insert(data) {
     if (data < this.data && this.left) {
-		this.left.insert(data);
+	this.left.insert(data);
     } else if (data < this.data) {
-		let newNode = new Bst(data);
-		this.left = newNode;
+	let newNode = new Bst(data);
+	this.left = newNode;
     } else if (data > this.data && this.right) {
-		this.right.insert(data);
+	this.right.insert(data);
     } else if (data > this.data) {
-		let newNode = new Bst(data);
-		this.right = newNode;
+	let newNode = new Bst(data);
+	this.right = newNode;
     }
   }
 
   contains(data) {
     if (this.data === data) {
-		return this;
+	return this;
     }
 
     if (this.data < data && this.right) {
-		return this.right.contains(data);
+	return this.right.contains(data);
     } else if (this.data > data && this.left) {
-		return this.left.contains(data);
+	return this.left.contains(data);
     }
     return null;
   }
